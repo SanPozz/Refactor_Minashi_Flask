aa0afe4a255e82aed3ada854a6cccfc9202b75c5
Santiago Pozzolo <pozzolosanti@gmail.com>
2025-10-07 03:04:01 -0300

    Login y Register Finalizados

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_bcrypt import Bcrypt
+from flask_login import (
+    LoginManager, login_user,
+    logout_user, login_required, current_user
+)
 from database import db, init_db
 from routes.venta_minerales import venta_minerales_bp
 from routes.pedidos import pedidos_bp
 from routes.carrito import carrito_bp
 
 app = Flask(__name__)
-# Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minashi.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Minashi-flask'
@@ -13,6 +17,12 @@ app.secret_key = 'Minashi-flask'
 from models.User import User
 
 init_db(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 app.register_blueprint(venta_minerales_bp)
 app.register_blueprint(pedidos_bp)
@@ -30,6 +40,8 @@ def register():
         mail = request.form['mail']
         confirm_password = request.form['confirm_password']
 
+        passFormatted = Bcrypt().generate_password_hash(password).decode('utf-8')
+
         if password != confirm_password:
             return render_template('register.html', error='Las contraseñas no coinciden')
         
@@ -39,7 +51,7 @@ def register():
         if User.query.filter_by(mail=mail).first():
             return render_template('register.html', error='El correo ya está registrado')
         
-        new_user = User(username=username, password=password, mail=mail, role='user')
+        new_user = User(username=username, password=passFormatted, mail=mail, role='user')
         db.session.add(new_user)
         db.session.commit()
 
@@ -49,7 +61,7 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     
-    if 'user' in session:
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     
     if request.method == 'POST':
@@ -57,12 +69,18 @@ def login():
         password = request.form['password']
 
         userDB = User.query.filter_by(username=username).first()
-        if userDB and userDB.password == password:
-            session['user'] = username
+        if userDB and Bcrypt().check_password_hash(userDB.password, password):
+            login_user(userDB)
             return redirect(url_for('home'))
         else:
             return render_template('login.html', error='Credenciales invalidas')
     return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- routes/carrito.py
+++ routes/carrito.py
@@ -4,8 +4,8 @@ carrito_bp = Blueprint('carrito', __name__)
 @carrito_bp.route('/carrito', methods=['GET', 'POST'])
 def carrito():
     if request.method == 'POST':
-        # Lógica para actualizar el carrito (agregar, eliminar, modificar cantidades)
+
         return redirect(url_for('carrito.carrito'))
     
-    # Aquí iría la lógica para obtener y mostrar los items en el carrito
+
     return render_template('carrito.html')
