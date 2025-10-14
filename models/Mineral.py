2985df63e7074ee5a2ac45d2b630b94258da2543
Santiago Pozzolo <pozzolosanti@gmail.com>
2025-10-14 14:42:50 -0300

    Prueba de API externa

---
+++ models/Mineral.py
@@ -0,0 +1,24 @@
+from database import db
+
+class Mineral(db.Model):
+    __tablename__ = 'minerals'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(250), nullable=True)
+
+    def __repr__(self):
+        return f"Mineral('{self.name}', '{self.price}')"
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'stock': self.stock,
+            'image_url': self.image_url
+        }
--- routes/venta_minerales.py
+++ routes/venta_minerales.py
@@ -1,8 +1,40 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from dotenv import load_dotenv;
+import os,json;
+from flask import current_app
+import requests;
+
+load_dotenv();
+
+API_KEY = os.getenv('API_KEY');
+URL_API = os.getenv('URL_API');
+
 
 venta_minerales_bp = Blueprint('venta_minerales', __name__)
 
 @venta_minerales_bp.route('/comprar_minerales')
 def comprar_minerales():
-    
-    return render_template('comprar_minerales.html')
+
+    url = f"{URL_API}?api_key={API_KEY}&unit=kg&currency=ARS"
+
+    headers = {}
+
+    headers["Accept"] = "application/json"
+
+    # resp = requests.get(url, headers=headers)
+    # data = resp.json()
+
+    data = load_json_from_file()
+
+    currency = data['currency']
+    metals = data['metals']
+
+    return render_template('comprar_minerales.html', metals=metals, currency=currency)
+
+def load_json_from_file():
+
+    path = os.path.join(current_app.instance_path, 'apiResponse.json')
+    path = os.path.normpath(path)
+    with open(path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
