56003bf9333f43b667909f3791c69d560871398b
Sebasonacht <sebasonacht@gmail.com>
2025-10-16 14:15:58 -0300

    update

---
+++ models/Order.py
@@ -0,0 +1,10 @@
+from database import db
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(50), default='pending')
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
