82c5c6253336a7eb4d1033dff1961786677468c0
Santiago Pozzolo <pozzolosanti@gmail.com>
2025-10-16 14:17:40 -0300

    Empezando Carrito

---
+++ Classes/Mineral_Carrito.py
@@ -0,0 +1,26 @@
+class MineralCarrito:
+    def __init__(self, nombre, precio, cantidad):
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+    def to_dict(self):
+        return {
+            'nombre': self.nombre,
+            'precio': self.precio,
+            'cantidad': self.cantidad
+        }
+    def __repr__(self):
+        return f"MineralCarrito('{self.nombre}', '{self.precio}', '{self.cantidad}')"
+
+    def actualizar_cantidad(self, nueva_cantidad):
+        self.cantidad = nueva_cantidad
+
+    def calcular_total(self):
+        return self.precio * self.cantidad
+
+    def incrementar_cantidad(self, incremento=1):
+        self.cantidad += incremento
+
+    def decrementar_cantidad(self, decremento=1):
+        if self.cantidad - decremento >= 0:
+            self.cantidad -= decremento
