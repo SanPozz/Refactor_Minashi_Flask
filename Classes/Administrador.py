e44a46fe146064ea6e979ea7dcc4cd17dc0eac57
Sebasonacht <sebasonacht@gmail.com>
2025-10-27 10:43:42 -0300

    Clases usuario

---
+++ Classes/Administrador.py
@@ -0,0 +1,8 @@
+class Administrador(Usuario):
+    def __init__(self, username, mail, password, rol, id_admin):
+        super().__init__(username, mail, password, rol)
+        self.id_admin = id_admin
+
+    def mostrar_informacion(self):
+        info_usuario = super().mostrar_informacion()
+        return f"{info_usuario}, ID Administrador: {self.id_admin}"
